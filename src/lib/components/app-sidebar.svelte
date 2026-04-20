e715ab262846026c52f3ec5e04ef1b303cfebed3
Michael Maerten <mike94100@gmail.com>
2026-04-20 10:19:17 -0400

    Refactored app-sidebar.svelte to simplify changes. Sections are typed for conformity, listed in a single array that is looped to simplify code.

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -2,357 +2,124 @@
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import { base } from "$app/paths";
 
-    //General items,
-    const General = [
-        {
-            title: "Home",
-            url: "/",
-        },
-        {
-            title: "Direct Downloads",
-            url: base + "/General/ddl",
-        },
-        {
-            title: "Torrents",
-            url: base + "/General/torrents",
-        },
-        {
-            title: "Preservation",
-            url: base + "/General/preservation",
-        },
-        {
-            title: "Other",
-            url: base + "/General/other",
-        },
-    ];
-    //Popular items,
-    const populars = [
-        {
-            title: "About RVZ files",
-            url: base + "/Populars/about-rvz-files",
-        },
-        {
-            title: "Metroid",
-            url: base + "/Populars/metroid",
-        },
-        {
-            title: "Mario",
-            url: base + "/Populars/mario",
-        },
-        {
-            title: "Fire Emblem",
-            url: base + "/Populars/fire-emblem",
-        },
-        {
-            title: "The Legend of Zelda",
-            url: base + "/Populars/zelda",
-        },
-        {
-            title: "Pokemon",
-            url: base + "/Populars/pokemon",
-        },
-        {
-            title: "Persona",
-            url: base + "/Populars/persona",
-        },
-        {
-            title: "Super Smash Bros",
-            url: base + "/Populars/super-smash-bros",
-        },
-        {
-            title: "Xenoblade",
-            url: base + "/Populars/xenoblade",
-        },
-    ];
+    type SidebarItem = {
+        title: string;
+        url: string;
+    };
 
-    //Nintendo items,
-    const Nintendo = [
-        {
-            title: "Nintendo Games",
-            url: base + "/Nintendo/nintendo-games",
-        },
-        {
-            title: "Nintendo DS",
-            url: base + "/Nintendo/nintendo-ds",
-        },
-        {
-            title: "Nintendo 3DS",
-            url: base + "/Nintendo/nintendo-3ds",
-        },
-        {
-            title: "Nintendo GameCube",
-            url: base + "/Nintendo/nintendo-gamecube",
-        },
-        {
-            title: "Nintendo Wii",
-            url: base + "/Nintendo/nintendo-wii",
-        },
-        {
-            title: "Nintendo Wii U",
-            url: base + "/Nintendo/nintendo-wii-u",
-        },
-    ];
+    type SidebarSection = {
+        label: string;
+        items: SidebarItem[];
+    };
 
-    //Sony items,
-    const sony = [
+    const sidebarSections: SidebarSection[] = [
         {
-            title: "NoPayStation",
-            url: base + "/Sony/nopaystation",
+            label: "General",
+            items: [
+                { title: "Home", url: "/" },
+                { title: "Direct Downloads", url: base + "/General/ddl" },
+                { title: "Torrents", url: base + "/General/torrents" },
+                { title: "Preservation", url: base + "/General/preservation" },
+                { title: "Other", url: base + "/General/other" },
+            ]
         },
         {
-            title: "Sony Playstation",
-            url: base + "/Sony/sony-playstation",
+            label: "Popular",
+            items: [
+                { title: "About RVZ files", url: base + "/Populars/about-rvz-files" },
+                { title: "Metroid", url: base + "/Populars/metroid" },
+                { title: "Mario", url: base + "/Populars/mario" },
+                { title: "Fire Emblem", url: base + "/Populars/fire-emblem" },
+                { title: "The Legend of Zelda", url: base + "/Populars/zelda" },
+                { title: "Pokemon", url: base + "/Populars/pokemon" },
+                { title: "Persona", url: base + "/Populars/persona" },
+                { title: "Super Smash Bros", url: base + "/Populars/super-smash-bros" },
+                { title: "Xenoblade", url: base + "/Populars/xenoblade" },
+            ]
         },
         {
-            title: "Sony Playstation 2",
-            url: base + "/Sony/sony-playstation-2",
+            label: "Nintendo",
+            items: [
+                { title: "Nintendo Games", url: base + "/Nintendo/nintendo-games" },
+                { title: "Nintendo DS", url: base + "/Nintendo/nintendo-ds" },
+                { title: "Nintendo 3DS", url: base + "/Nintendo/nintendo-3ds" },
+                { title: "Nintendo GameCube", url: base + "/Nintendo/nintendo-gamecube" },
+                { title: "Nintendo Wii", url: base + "/Nintendo/nintendo-wii" },
+                { title: "Nintendo Wii U", url: base + "/Nintendo/nintendo-wii-u" },
+            ]
         },
         {
-            title: "Sony Playstation Portable",
-            url: base + "/Sony/sony-playstation-portable",
+            label: "Sony",
+            items: [
+                { title: "NoPayStation", url: base + "/Sony/nopaystation" },
+                { title: "Sony Playstation", url: base + "/Sony/sony-playstation" },
+                { title: "Sony Playstation 2", url: base + "/Sony/sony-playstation-2" },
+                { title: "Sony Playstation Portable", url: base + "/Sony/sony-playstation-portable" },
+                { title: "Sony Playstation 3", url: base + "/Sony/sony-playstation-3" },
+                { title: "Sony Playstation Vita", url: base + "/Sony/sony-playstation-vita" },
+            ]
         },
         {
-            title: "Sony Playstation 3",
-            url: base + "/Sony/sony-playstation-3",
+            label: "Microsoft",
+            items: [
+                { title: "Microsoft Xbox", url: base + "/Microsoft/microsoft-xbox" },
+                { title: "Microsoft Xbox 360", url: base + "/Microsoft/microsoft-xbox360" },
+            ]
         },
         {
-            title: "Sony Playstation Vita",
-            url: base + "/Sony/sony-playstation-vita",
-        },
-    ];
-
-    //Microsoft items,
-    const microsoft = [
-        {
-            title: "Microsoft Xbox",
-            url: base + "/Microsoft/microsoft-xbox",
+            label: "Sega",
+            items: [
+                { title: "Sega Games", url: base + "/Sega/sega-games" },
+                { title: "Sega Dreamcast", url: base + "/Sega/sega-dreamcast" },
+                { title: "Sega Saturn", url: base + "/Sega/sega-saturn" },
+                { title: "Sega CD", url: base + "/Sega/sega-cd" },
+            ]
         },
         {
-            title: "Microsoft Xbox 360",
-            url: base + "/Microsoft/microsoft-xbox360",
-        },
-    ];
-
-    //Sega items,
-    const sega = [
-        {
-            title: "Sega Games",
-            url: base + "/Sega/sega-games",
+            label: "Retro & Arcade",
+            items: [
+                { title: "Retro Games", url: base + "/Retro & Arcade/retro-games" },
+                { title: "Arcade", url: base + "/Retro & Arcade/arcade" },
+            ]
         },
         {
-            title: "Sega Dreamcast",
-            url: base + "/Sega/sega-dreamcast",
+            label: "PC",
+            items: [
+                { title: "PC Games", url: base + "/PC/pc-games" },
+            ]
         },
         {
-            title: "Sega Saturn",
-            url: base + "/Sega/sega-saturn",
-        },
-        {
-            title: "Sega CD",
-            url: base + "/Sega/sega-cd",
-        },
-    ];
-
-    // Retro & Arcade items,
-    const retroArcade = [
-        {
-            title: "Retro Games",
-            url: base + "/Retro & Arcade/retro-games",
-        },
-        {
-            title: "Arcade",
-            url: base + "/Retro & Arcade/arcade",
-        },
-    ];
-
-    //PC items,
-    const pc = [
-        {
-            title: "PC Games",
-            url: base + "/PC/pc-games",
-        },
-    ];
-
-    // Miscellaneous items,
-    const misc = [
-        {
-            title: "ROM Sets",
-            url: base + "/Miscellaneous/rom-sets",
-        },
-        {
-            title: "BIOS Sets",
-            url: base + "/Miscellaneous/bios-sets",
-        },
-        {
-            title: "Other",
-            url: base + "/Miscellaneous/other",
-        },
+            label: "Miscellaneous",
+            items: [
+                { title: "ROM Sets", url: base + "/Miscellaneous/rom-sets" },
+                { title: "BIOS Sets", url: base + "/Miscellaneous/bios-sets" },
+                { title: "Other", url: base + "/Miscellaneous/other" },
+            ]
+        }
     ];
 </script>
 
 <Sidebar.Root collapsible="offcanvas">
     <Sidebar.Content>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>General</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each General as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Popular</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each populars as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Nintendo</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each Nintendo as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Sony</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each sony as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Microsoft</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each microsoft as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Sega</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each sega as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Retro & Arcade</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each retroArcade as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>PC</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each pc as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Miscellaneous</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each misc as item}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
+        {#each sidebarSections as section}
+            <Sidebar.Group>
+                <Sidebar.GroupLabel>{section.label}</Sidebar.GroupLabel>
+                <Sidebar.GroupContent>
+                    <Sidebar.Menu>
+                        {#each section.items as item}
+                            <Sidebar.MenuItem>
+                                <Sidebar.MenuButton>
+                                    {#snippet child({ props })}
+                                        <a href={item.url} {...props}>
+                                            <span>{item.title}</span>
+                                        </a>
+                                    {/snippet}
+                                </Sidebar.MenuButton>
+                            </Sidebar.MenuItem>
+                        {/each}
+                    </Sidebar.Menu>
+                </Sidebar.GroupContent>
+            </Sidebar.Group>
+        {/each}
     </Sidebar.Content>
 </Sidebar.Root>
