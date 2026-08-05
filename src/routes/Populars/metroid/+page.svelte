66454e52ba4354564500d8b3f5ed826446f5fc81
SuperBio <SuperBionicleKing@gmail.com>
2026-08-05 13:56:54 -0600

    Add download links for Metroid games

--- src/routes/Populars/metroid/+page.svelte
+++ src/routes/Populars/metroid/+page.svelte
@@ -20,6 +20,7 @@
                 <TableRow>
                     <TableHead>Title</TableHead>
                     <TableHead>Minerva (Torrent Based)</TableHead>
+                    <TableHead>Internet Archive</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
@@ -28,6 +29,13 @@
                     <TableCell>
                         <a
                             href="https://minerva-archive.org/browse/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/Metroid%20Fusion%20(USA).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/No-Intro_GameBoy_Advance_Collection/mnt/user/ROMS/Nintendo/GBA_2/GBA.zip/GBA%2FMetroid%20Fusion%20%28USA%29.zip"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -42,12 +50,9 @@
                             >Download</a
                         >
                     </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell>Metroid Prime Trilogy (Wii) (USA)</TableCell>
                     <TableCell>
                         <a
-                            href="https://minerva-archive.org/browse/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%20Trilogy%20(USA).zip"
+                            href="https://archive.org/download/No-Intro_GameBoy_Advance_Collection/mnt/user/ROMS/Nintendo/GBA_2/GBA.zip/GBA%2FMetroid%20-%20Zero%20Mission%20%28USA%29.zip"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
@@ -62,6 +67,13 @@
                             >Download</a
                         >
                     </TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/GameCube-Redump-2024-Individual-Games-Part1/Metroid%20Prime%20%28USA%29.zip/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Metroid Prime 2 (GC) (USA)</TableCell>
@@ -72,12 +84,54 @@
                             >Download</a
                         >
                     </TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/GameCube-Redump-2024-Individual-Games-Part1/Metroid%20Prime%202%20-%20Echoes%20%28USA%29.zip/"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Metroid Prime 3 (Wii) (USA)</TableCell>
                     <TableCell>
                         <a
                             href="https://minerva-archive.org/browse/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%203%20-%20Corruption%20(USA).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/Wii-Redump-2024-Individual-Games-Part4/Metroid%20Prime%203%20-%20Corruption%20%28USA%29.zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Metroid Prime Trilogy (Wii) (USA)</TableCell>
+                    <TableCell>
+                        <a
+                            href="https://minerva-archive.org/browse/Redump/Nintendo%20-%20Wii%20-%20NKit%20RVZ%20[zstd-19-128k]/Metroid%20Prime%20Trilogy%20(USA).zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/Wii-Redump-2024-Individual-Games-Part4/Metroid%20Prime%20Trilogy%20%28USA%29.zip"
+                            class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
+                            >Download</a
+                        >
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Metroid - Samus Returns (3DS) (USA)</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                        <a
+                            href="https://archive.org/download/3DS-Redump-2024-Individual-Games-Part1/Metroid%20-%20Samus%20Returns%20%28USA%29%20%28En%2CFr%2CEs%29.zip"
                             class="text-primary font-medium underline underline-offset-4 md:text-base hover:bg-primary hover:text-primary-foreground"
                             >Download</a
                         >
